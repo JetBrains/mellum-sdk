6c3444409f2a12560e524fa348f1de4a1bd1a054
Egor Safronov <egor.safronov@jetbrains.com>
2025-07-21 16:01:04 +0300

    for releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,8 @@
 group = "ai.jetbrains.mellum.sdk"
 version = "0.0.1"
+
+tasks.register("reportProjectVersionToTeamCity") {
+    doLast {
+        println("##teamcity[buildNumber '${project.version}']")
+    }
+}
