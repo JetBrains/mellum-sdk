dc3f85392e158581910c98add8647c955c78742f
Egor Safronov <egor.safronov@jetbrains.com>
2025-07-21 14:55:39 +0300

    fixup! publishing

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation(libs.ktor.client.logging)
     implementation(libs.ktor.serialization.kotlinx.json)
 
-    testImplementation(kotlin("test"))
+    testImplementation(libs.kotlin.test)
     implementation(libs.kotlin.logging)
     testImplementation(libs.slf4j.api)
     testRuntimeOnly(libs.logback.classic)  // Added logback as the logging implementation
